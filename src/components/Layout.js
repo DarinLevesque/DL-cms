c81e6e0c8e798ade8261bf0f923cd0edad295abf
Darin Levesque <darin@darinlevesque.com>
2018-10-12 14:20:07 -0400

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Darin Levesque" />
     <Navbar />
     <div>{children}</div>
   </div>
