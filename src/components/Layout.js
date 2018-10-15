502b72eb42a8755078a3528882405b5f92741612
Darin Levesque <darin@darinlevesque.com>
2018-10-15 12:42:51 -0400

    Added Header, Footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,13 +2,39 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
+import Header from '../components/Header'
+import Footer from '../components/Footer';
 import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Darin Levesque" />
-    <Navbar />
-    <div>{children}</div>
+    <Helmet>
+      <html lang="en" />
+      <title>Digital Resume for Darin Levesque</title>
+      <script
+        defer
+        src="https://use.fontawesome.com/releases/v5.0.12/js/all.js"
+        integrity="sha384-Voup2lBiiyZYkRto2XWqbzxHXwzcm4A5RfdfG6466bu5LqjwwrjXCMBQBLMWh7qR"
+        crossorigin="anonymous"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
+        integrity="sha384-OHBBOqpYHNsIqQy8hL1U+8OXf9hH6QRxi0+EODezv82DfnZoV7qoHAZDwMwEJvSw"
+        crossorigin="anonymous"
+      />
+    </Helmet>
+  
+    <div id="outer-container">
+      <Navbar />
+      <main className="site" id="page-wrap">
+        <div className="site-content">
+          <Header />
+          <div>{children()}</div>
+        </div>
+        <Footer />
+      </main>
+    </div>
   </div>
 )
 
