549a2fcfe83c59a8038b9bdee9c56d6beed8a780
Darin Levesque <darin@darinlevesque.com>
2018-10-16 09:30:45 -0400

    added smartypants

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-smartypants`,
           {
             resolve: `gatsby-remark-images`,
             options: {
