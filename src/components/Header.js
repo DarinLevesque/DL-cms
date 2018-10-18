999b13d585d5a3e23bc8c6397271ce98c31d4f54
Darin Levesque <darin@darinlevesque.com>
2018-10-18 15:17:10 -0400

    added .container to navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,54 +8,56 @@ const Header = () => {
   return (
     <Headroom>
       <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <Link className="navbar-item" to="/">
-            <img src={logo} alt="Darin Levesque Logo" width="112" height="50"/>
-          </Link>
-        </div>
-        <div className="navbar-menu">
-          <div className="navbar-start"></div>
-          <div className="navbar-end">
-            <Link to="/about" className="navbar-item is-active">
-              <i className="fas fa-user"/>{" "}About
+        <div class="container">
+          <div className="navbar-brand">
+            <Link className="navbar-item" to="/">
+              <img src={logo} alt="Darin Levesque Logo" width="112" height="50"/>
             </Link>
-            <div className="navbar-item has-dropdown is-hoverable">
-              <Link to="/blog" className="navbar-link">
-                Blog
+          </div>
+          <div className="navbar-menu">
+            <div className="navbar-start"></div>
+            <div className="navbar-end">
+              <Link to="/about" className="navbar-item is-active">
+                <i className="fas fa-user"/>{" "}About
               </Link>
-              <div className="navbar-dropdown">
-                <Link to="/blog/professional" className="navbar-item">
-                  <i className="fas fa-fighter-jet"/>{" "}
-                  Professional
+              <div className="navbar-item has-dropdown is-hoverable">
+                <Link to="/blog" className="navbar-link">
+                  Blog
                 </Link>
-                <Link to="/blog/family" className="navbar-item">
-                  <i className="fas fa-users"/>{" "}Family
-                </Link>
+                <div className="navbar-dropdown">
+                  <Link to="/blog/professional" className="navbar-item">
+                    <i className="fas fa-fighter-jet"/>{" "}
+                    Professional
+                  </Link>
+                  <Link to="/blog/family" className="navbar-item">
+                    <i className="fas fa-users"/>{" "}Family
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div className="navbar-item has-dropdown is-hoverable">
-              <Link to="/resume" className="navbar-link">
-                Resume
+              <div className="navbar-item has-dropdown is-hoverable">
+                <Link to="/resume" className="navbar-link">
+                  Resume
+                </Link>
+                <div className="navbar-dropdown">
+                  <Link to="/resume/experience" className="navbar-item">
+                    Experience
+                  </Link>
+                  <Link to="/resume/education" className="navbar-item">
+                    Education
+                  </Link>
+                  <Link to="/resume/skills" className="navbar-item">
+                    Skills
+                  </Link>
+                  <hr className="navbar-divider"/>
+                  <Link to="/timeline" className="navbar-item">
+                    <i className="fas fa-download"/>{" "}Download
+                  </Link>
+                </div>
+              </div>
+              <Link to="/contact" className="navbar-item">
+                Contact
               </Link>
-              <div className="navbar-dropdown">
-                <Link to="/resume/experience" className="navbar-item">
-                  Experience
-                </Link>
-                <Link to="/resume/education" className="navbar-item">
-                  Education
-                </Link>
-                <Link to="/resume/skills" className="navbar-item">
-                  Skills
-                </Link>
-                <hr className="navbar-divider"/>
-                <Link to="/timeline" className="navbar-item">
-                  <i className="fas fa-download"/>{" "}Download
-                </Link>
-              </div>
             </div>
-            <Link to="/contact" className="navbar-item">
-              Contact
-            </Link>
           </div>
         </div>
       </nav>
