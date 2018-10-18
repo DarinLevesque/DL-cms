20e21aedd146d0ce01c8f4f3c18fe74276045639
Darin Levesque <darin@darinlevesque.com>
2018-10-18 15:39:20 -0400

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import PageTransition from 'gatsby-plugin-page-transitions'
 
 export default class BlogPage extends React.Component {
   render() {
@@ -10,37 +11,39 @@ export default class BlogPage extends React.Component {
 
     return (
       <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Articles</h1>
+        <PageTransition>
+          <section className="section">
+            <div className="container">
+              <div className="content">
+                <h1 className="has-text-weight-bold is-size-2">Latest Articles</h1>
+              </div>
+              {posts
+                .map(({ node: post }) => (
+                  <div
+                    className="content"
+                    style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
+                    key={post.id}
+                  >
+                    <p>
+                      <Link className="has-text-primary" to={post.fields.slug}>
+                        {post.frontmatter.title}
+                      </Link>
+                      <span> &bull; </span>
+                      <small>{post.frontmatter.date}</small>
+                    </p>
+                    <p>
+                      {post.excerpt}
+                      <br />
+                      <br />
+                      <Link className="button is-small" to={post.fields.slug}>
+                        Keep Reading →
+                      </Link>
+                    </p>
+                  </div>
+                ))}
             </div>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                  key={post.id}
-                >
-                  <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
-                  </p>
-                  <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
-                  </p>
-                </div>
-              ))}
-          </div>
-        </section>
+          </section>
+        </PageTransition>
       </Layout>
     )
   }
