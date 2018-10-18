b28fde002a48aa37a9e398995946b6c23762876e
Darin Levesque <darin@darinlevesque.com>
2018-10-18 14:24:08 -0400

    Switched to index rather than blog as root route

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,78 +2,98 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import Typed from '../components/Typed'
 
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
-
     return (
-      <Layout>
+      <div>
+        <section className="hero is-medium is-dark is-bold">
+          <div className="hero-body">
+            <div className="container">
+              <article className="media columns">
+                <figure
+                  className="media-left column is-offset-one-quarter is-one-quarter animated fadeInLeftBig">
+                  <p className="image is-128x128">
+                    <img src={Profile} />
+                  </p>
+                </figure>
+                <div className="media-content column is-one-half">
+                  <h1 className="title">Who am I?</h1>
+                  <h2 className="subtitle">
+                    <Typed
+                      strings={["Hi, my name is Darin.", "I'm a Test Resource Manager for NAVAIR"]}
+                      startDelay={2500}
+                      nextStringDelay={[2500, 2000]}
+                      lifeLike={true}
+                      cursorChar="_"
+                      breakLines={false}
+                      autoStart={false} />
+                  </h2>
+                </div>
+              </article>
+            </div>
+          </div>
+        </section>
         <section className="section">
           <div className="container">
             <div className="content">
-              <h1 className="has-text-weight-bold is-size-2">Latest Articles</h1>
-            </div>
-            {posts
-              .map(({ node: post }) => (
-                <div
-                  className="content"
-                  style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                  key={post.id}
-                >
+              <div className="columns is-mobile is-centered">
+                <div className="column is-two-thirds">
+                  <h1>Welcome</h1>
                   <p>
-                    <Link className="has-text-primary" to={post.fields.slug}>
-                      {post.frontmatter.title}
-                    </Link>
-                    <span> &bull; </span>
-                    <small>{post.frontmatter.date}</small>
+                    I've been working in high-demand aviation environments since 2004. {" "}<Link to="/about">My journey</Link>{" "}
+                    started as a CH-46 Seaknight (Phrog) Crew Chief after which I transtioned to the
+                    V-22 Osprey in 2006 and decided to enter government civilian service in 2014.
                   </p>
                   <p>
-                    {post.excerpt}
-                    <br />
-                    <br />
-                    <Link className="button is-small" to={post.fields.slug}>
-                      Keep Reading →
-                    </Link>
+                    In my ten years on active duty in the Marine Corps I achieved the following:
+                  </p>
+                  <ul>
+                    <li>Weapons and Tactics Instructor</li>
+                    <li>Night Systems Instructor</li>
+                    <li>Crew Resource Management Instructor</li>
+                    <li>
+                      Naval Air Training & Operating Procedures Standardization Instructor
+                    </li>
+                    <li>Collateral Duty Inspector</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container is-widescreen"></div>
+        </section>
+        <section className="section">
+          <div className="columns">
+            <div className="column is-one-third">
+              <div className="card">
+                <div className="card-content">
+                  <p className="title">
+                    "Quotes from Twitter"
+                  </p>
+                  <p className="subtitle">
+                    Author
                   </p>
                 </div>
-              ))}
+                <footer className="card-footer">
+                  <p className="card-footer-item">
+                    <span>
+                      View on
+                      <a href="https://twitter.com/darinlevesque">Twitter</a>
+                    </span>
+                  </p>
+                  <p className="card-footer-item">
+                    Share
+                  </p>
+                </footer>
+              </div>
+            </div>
+            <div className="column"></div>
+            <div className="columns"></div>
           </div>
         </section>
-      </Layout>
+      </div>
     )
   }
 }
-
-IndexPage.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
-    ) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-          }
-        }
-      }
-    }
-  }
-`
