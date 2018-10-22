37cbfb31cb74cf0dc2a64d0c2ba2bd8452fac1ab
Darin Levesque <darin@darinlevesque.com>
2018-10-22 09:34:45 -0400

    404 image

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import 404 from '../img/404.png'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
+    <div className="columns">
+      <div className="column">
+        <img src={404} />
+      </div>
+      <div className="column">
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
     </div>
   </Layout>
 )
