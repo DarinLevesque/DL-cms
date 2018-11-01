1608134526ca69a445b7e8d52bfc645b6fb07520
Darin Levesque <darin@darinlevesque.com>
2018-11-01 16:25:00 -0400

    Update index.js

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -1,57 +1,59 @@
 import React from "react";
 import Layout from '../../components/Layout'
 
-const Index = () => {
+function openTab(evt, tabTitle) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabTitle).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+export default class ResumePage extends React.Component {
+  render() {
     return (
         <Layout>
             <section className="section columns">
                 <div className="column">
-                    <div class="tabs is-toggle is-fullwidth" id="tabs">
-                      <ul>
-                        <li class="is-active" data-tab="1">
-                          <a>
-                            <span class="icon is-small"><i class="fa fa-image"></i></span>
-                            <span>Pictures</span>
+                    <div class="tabs is-centered">
+                        <ul>
+                          <li id="tablinks">
+                            <a onclick="openTab(event, 'Overview')">
+                            <span class="icon is-large"><i class="fa fa-2x fa-html5 blue"></i></span>
+                            <span>Overview</span>
                           </a>
-                        </li>
-                        <li data-tab="2">
-                          <a>
-                            <span class="icon is-small"><i class="fa fa-music"></i></span>
-                            <span>Music</span>
+                          </li>
+                          <li id="tablinks">
+                            <a onclick="openTab(event, 'Details')">
+                            <span class="icon is-large"><i class="fa fa-2x fa-tablet blue"></i></span>
+                            <span>Details</span>
                           </a>
-                        </li>
-                        <li data-tab="3">
-                          <a>
-                            <span class="icon is-small"><i class="fa fa-film"></i></span>
-                            <span>Videos</span>
+                          </li>
+                          <li id="tablinks">
+                            <a onclick="openTab(event, 'Specification')">
+                            <span class="icon is-large"><i class="fa fa-2x fa-file-code-o blue"></i></span>
+                            <span>Specification</span>
                           </a>
-                        </li>
-                        <li data-tab="4">
-                          <a>
-                            <span class="icon is-small"><i class="fa fa-file-text-o"></i></span>
-                            <span>Documents</span>
+                          </li>
+                          <li id="tablinks">
+                             <a onclick="openTab(event, 'Reviews')">
+                            <span class="icon is-large"><i class="fa fa-2x fa-cog blue"></i></span>
+                            <span>Reviews</span>
                           </a>
-                        </li>
-                      </ul>
-                    </div>
-                    <div id="tab-content">
-                      <p class="is-active" data-content="1">
-                        Pictures
-                      </p>
-                      <p data-content="2">
-                        Music
-                      </p>
-                      <p data-content="3">
-                        Videos
-                      </p>
-                      <p data-content="4">
-                        Documents
-                      </p>
-                    </div>
+                          </li>
+                        </ul>
+                        <!--/tabs is-centered-->
+                      </div>
                 </div>
             </section>
         </Layout>
     );
 };
 
-export default Index
+export default ResumePage
