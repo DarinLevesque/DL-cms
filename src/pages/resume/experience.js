8a3df19737ffa0ae786233c58423ab26713a37d1
Darin Levesque <darin@darinlevesque.com>
2018-11-01 12:44:33 -0400

    Update experience.js

--- src/pages/resume/experience.js
+++ src/pages/resume/experience.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Layout from '../../components/Layout'
+import TimeAgo from 'react-timeago'
 
 const Experience = () => {
     return (
         <Layout>
             <section className="section columns">
                 <div className="column">
-
+                    <p>Test Resource Manager for NAVAIR hired <TimeAgo date="Sep 30, 2015" /></p>
                 </div>
             </section>
         </Layout>
