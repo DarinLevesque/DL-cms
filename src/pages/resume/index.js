66db67123a4967980040629ae77445392c26cf83
Darin Levesque <darin@darinlevesque.com>
2018-11-05 14:39:12 -0500

    added closed curly bracket

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -17,40 +17,41 @@ function openTab(evt, tabTitle) {
 
 export default class ResumePage extends React.Component {
   render() {
-    return (
-        <Layout>
-            <section className="section columns">
-                <div className="column">
-                    <div class="tabs is-centered">
-                        <ul>
-                          <li id="tablinks">
-                            <a onclick="openTab(event, 'Overview')">
-                            <span class="icon is-large"><i class="fa fa-2x fa-html5 blue"></i></span>
-                            <span>Overview</span>
-                          </a>
-                          </li>
-                          <li id="tablinks">
-                            <a onclick="openTab(event, 'Details')">
-                            <span class="icon is-large"><i class="fa fa-2x fa-tablet blue"></i></span>
-                            <span>Details</span>
-                          </a>
-                          </li>
-                          <li id="tablinks">
-                            <a onclick="openTab(event, 'Specification')">
-                            <span class="icon is-large"><i class="fa fa-2x fa-file-code-o blue"></i></span>
-                            <span>Specification</span>
-                          </a>
-                          </li>
-                          <li id="tablinks">
-                             <a onclick="openTab(event, 'Reviews')">
-                            <span class="icon is-large"><i class="fa fa-2x fa-cog blue"></i></span>
-                            <span>Reviews</span>
-                          </a>
-                          </li>
-                        </ul>
-                      </div>
-                </div>
-            </section>
-        </Layout>
-    );
-};
+        return (
+            <Layout>
+                <section className="section columns">
+                    <div className="column">
+                        <div class="tabs is-centered">
+                            <ul>
+                              <li id="tablinks">
+                                <a onclick="openTab(event, 'Overview')">
+                                <span class="icon is-large"><i class="fa fa-2x fa-html5 blue"></i></span>
+                                <span>Overview</span>
+                              </a>
+                              </li>
+                              <li id="tablinks">
+                                <a onclick="openTab(event, 'Details')">
+                                <span class="icon is-large"><i class="fa fa-2x fa-tablet blue"></i></span>
+                                <span>Details</span>
+                              </a>
+                              </li>
+                              <li id="tablinks">
+                                <a onclick="openTab(event, 'Specification')">
+                                <span class="icon is-large"><i class="fa fa-2x fa-file-code-o blue"></i></span>
+                                <span>Specification</span>
+                              </a>
+                              </li>
+                              <li id="tablinks">
+                                 <a onclick="openTab(event, 'Reviews')">
+                                <span class="icon is-large"><i class="fa fa-2x fa-cog blue"></i></span>
+                                <span>Reviews</span>
+                              </a>
+                              </li>
+                            </ul>
+                          </div>
+                    </div>
+                </section>
+            </Layout>
+        )
+    }
+}
