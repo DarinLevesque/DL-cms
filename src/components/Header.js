cfeaaaf4a530f8bc88dc084dc4faddac4cc24dfa
Darin Levesque <darin@darinlevesque.com>
2018-11-08 15:37:09 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
           <div className="navbar-menu">
             <div className="navbar-start">
               <Link to="/about" className="navbar-item is-active">
-                <i className="fas fa-user" /><span> About</span>
+                <i className="fas fa-user" /><span>{' '} About</span>
               </Link>
               <div className="navbar-item has-dropdown is-hoverable">
                 <Link to="/blog" className="navbar-link">
@@ -30,7 +30,7 @@ const Header = () => {
                     <i className="fas fa-fighter-jet" /><span> Professional</span>
                   </Link>
                   <Link to="/blog/family" className="navbar-item">
-                    <i className="fas fa-users" /><span> Family</span>
+                    <i className="fas fa-users" /><span>{' '} Family</span>
                   </Link>
                 </div>
               </div>
@@ -40,17 +40,17 @@ const Header = () => {
                 </Link>
                 <div className="navbar-dropdown">
                   <Link to="/resume/experience" className="navbar-item">
-                    <i className="fas fa-wrench" /><span> Experience</span>
+                    <i className="fas fa-wrench" /><span>{' '} Experience</span>
                   </Link>
                   <Link to="/resume/education" className="navbar-item">
-                    <i className="fas fa-user-graduate" /><span> Education</span>
+                    <i className="fas fa-user-graduate" /><span>{' '} Education</span>
                   </Link>
                   <Link to="/resume/skills" className="navbar-item">
-                    <i className="fas fa-table-tennis" /><span> Skills</span>
+                    <i className="fas fa-table-tennis" /><span>{' '} Skills</span>
                   </Link>
                   <hr className="navbar-divider" />
                   <Link to="/timeline" className="navbar-item">
-                    <i className="fas fa-download" /><span> Download</span>
+                    <i className="fas fa-download" /><span>{' '} Download</span>
                   </Link>
                 </div>
               </div>
