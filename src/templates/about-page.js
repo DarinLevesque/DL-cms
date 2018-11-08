e31f401e92baa7b1b9c88d9ffe24725ca3a6a62c
Darin Levesque <darin@darinlevesque.com>
2018-11-08 14:56:48 -0500

    Update about-page.js

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -8,16 +8,12 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
-    <div className="container">
-      <div className="columns">
-        <div className="column is-10 is-offset-1">
-          <div className="section">
-            <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-              {title}
-            </h2>
-            <PageContent className="content" content={content} />
-          </div>
-        </div>
+    <div className="section">
+      <div className="container">
+        <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+          {title}
+        </h2>
+        <PageContent className="content" content={content} />
       </div>
     </div>
   )
