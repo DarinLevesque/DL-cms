eb9728d554cb06d246cd2b48e703e64c9a6a34ba
Darin Levesque <darin@darinlevesque.com>
2018-11-08 14:18:06 -0500

    Update education.js

--- src/pages/resume/education.js
+++ src/pages/resume/education.js
@@ -5,97 +5,99 @@ const Education = () => {
     return (
         <Layout>
             <section className="section">
-                <div class="tile is-ancestor">
-                  <div class="tile is-parent">
-                    <article class="tile is-child box">
-                      <p class="title">Hello World</p>
-                      <p class="subtitle">What is up?</p>
-                    </article>
-                  </div>
-                  <div class="tile is-parent">
-                    <article class="tile is-child box">
-                      <p class="title">Foo</p>
-                      <p class="subtitle">Bar</p>
-                    </article>
-                  </div>
-                  <div class="tile is-parent">
-                    <article class="tile is-child box">
-                      <p class="title">Third column</p>
-                      <p class="subtitle">With some content</p>
-                      <div class="content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-                      </div>
-                    </article>
-                  </div>
-                </div>
-
-                <div class="tile is-ancestor">
-                  <div class="tile is-vertical is-8">
-                    <div class="tile">
-                      <div class="tile is-parent is-vertical">
-                        <article class="tile is-child box">
-                          <p class="title">Vertical tiles</p>
-                          <p class="subtitle">Top box</p>
-                        </article>
-                        <article class="tile is-child box">
-                          <p class="title">Vertical tiles</p>
-                          <p class="subtitle">Bottom box</p>
+                <div className="container">
+                    <div className="tile is-ancestor">
+                      <div className="tile is-parent">
+                        <article className="tile is-child box">
+                          <p className="title">Hello World</p>
+                          <p className="subtitle">What is up?</p>
                         </article>
                       </div>
-                      <div class="tile is-parent">
-                        <article class="tile is-child box">
-                          <p class="title">Middle box</p>
-                          <p class="subtitle">With an image</p>
-                          <figure class="image is-4by3">
-                            <img src="https://bulma.io/images/placeholders/640x480.png" />
-                          </figure>
+                      <div className="tile is-parent">
+                        <article className="tile is-child box">
+                          <p className="title">Foo</p>
+                          <p className="subtitle">Bar</p>
+                        </article>
+                      </div>
+                      <div className="tile is-parent">
+                        <article className="tile is-child box">
+                          <p className="title">Third column</p>
+                          <p className="subtitle">With some content</p>
+                          <div className="content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                          </div>
                         </article>
                       </div>
                     </div>
-                    <div class="tile is-parent">
-                      <article class="tile is-child box">
-                        <p class="title">Wide column</p>
-                        <p class="subtitle">Aligned with the right column</p>
-                        <div class="content">
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
-                        </div>
-                      </article>
-                    </div>
-                  </div>
-                  <div class="tile is-parent">
-                    <article class="tile is-child box">
-                      <div class="content">
-                        <p class="title">Tall column</p>
-                        <p class="subtitle">With even more content</p>
-                        <div class="content">
-                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
-                          <p>Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula a sagittis. Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor lorem, et sollicitudin felis neque sit amet erat. Maecenas imperdiet felis nisi, fringilla luctus felis hendrerit sit amet. Aenean vitae gravida diam, finibus dignissim turpis. Sed eget varius ligula, at volutpat tortor.</p>
-                          <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
-                        </div>
-                      </div>
-                    </article>
-                  </div>
-                </div>
 
-                <div class="tile is-ancestor">
-                  <div class="tile is-parent">
-                    <article class="tile is-child box">
-                      <p class="title">Side column</p>
-                      <p class="subtitle">With some content</p>
-                      <div class="content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                    <div className="tile is-ancestor">
+                      <div className="tile is-vertical is-8">
+                        <div className="tile">
+                          <div className="tile is-parent is-vertical">
+                            <article className="tile is-child box">
+                              <p className="title">Vertical tiles</p>
+                              <p className="subtitle">Top box</p>
+                            </article>
+                            <article className="tile is-child box">
+                              <p className="title">Vertical tiles</p>
+                              <p className="subtitle">Bottom box</p>
+                            </article>
+                          </div>
+                          <div className="tile is-parent">
+                            <article className="tile is-child box">
+                              <p className="title">Middle box</p>
+                              <p className="subtitle">With an image</p>
+                              <figure className="image is-4by3">
+                                <img src="https://bulma.io/images/placeholders/640x480.png" />
+                              </figure>
+                            </article>
+                          </div>
+                        </div>
+                        <div className="tile is-parent">
+                          <article className="tile is-child box">
+                            <p className="title">Wide column</p>
+                            <p className="subtitle">Aligned with the right column</p>
+                            <div className="content">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                            </div>
+                          </article>
+                        </div>
                       </div>
-                    </article>
-                  </div>
-                  <div class="tile is-parent is-8">
-                    <article class="tile is-child box">
-                      <p class="title">Main column</p>
-                      <p class="subtitle">With some content</p>
-                      <div class="content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                      <div className="tile is-parent">
+                        <article className="tile is-child box">
+                          <div className="content">
+                            <p className="title">Tall column</p>
+                            <p className="subtitle">With even more content</p>
+                            <div className="content">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
+                              <p>Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula a sagittis. Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor lorem, et sollicitudin felis neque sit amet erat. Maecenas imperdiet felis nisi, fringilla luctus felis hendrerit sit amet. Aenean vitae gravida diam, finibus dignissim turpis. Sed eget varius ligula, at volutpat tortor.</p>
+                              <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque sed, gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
+                            </div>
+                          </div>
+                        </article>
                       </div>
-                    </article>
-                  </div>
+                    </div>
+
+                    <div className="tile is-ancestor">
+                      <div className="tile is-parent">
+                        <article className="tile is-child box">
+                          <p className="title">Side column</p>
+                          <p className="subtitle">With some content</p>
+                          <div className="content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                          </div>
+                        </article>
+                      </div>
+                      <div className="tile is-parent is-8">
+                        <article className="tile is-child box">
+                          <p className="title">Main column</p>
+                          <p className="subtitle">With some content</p>
+                          <div className="content">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+                          </div>
+                        </article>
+                      </div>
+                    </div>
                 </div>
             </section>
         </Layout>
