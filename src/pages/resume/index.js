1a3fc46fc6292bdea8f49a5bd81a3a02c96f4d24
Darin Levesque <darin@darinlevesque.com>
2018-11-14 16:13:21 -0500

    Update index.js

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -1,73 +1,105 @@
 import React from "react";
 import Layout from '../../components/Layout'
 
-function openTab(evt, tabName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("content-tab");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " is-active";
-  }
-
 export default class ResumePage extends React.Component {
-  openTab(evt, tabName) {
-        var i, x, tablinks;
-        x = document.getElementsByClassName("content-tab");
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
-        }
-        document.getElementById(tabName).style.display = "block";
-        evt.currentTarget.className += " is-active";
-      }   
+  componentDidMount() {
     
-    componentDidMount() {
-    const tabs = document.querySelectorAll(".tab")
-
-    tabs.forEach(tab => {
-      tab.addEventListener("click", event => {
-        document.getElementsByClassName("is-active")[0]
-                .classList.remove("is-active");
-
-        tab.classList.add("is-active")
-      })
-    })
   }
   render() {
         return (
             <Layout>
                 <section className="section columns">
-                    <div className="column">
-                        <div class="tabs">
-                          <ul>
-                            <li class="tab is-active" onclick="openTab(event,'Overview')"><a>Pictures</a></li>
-                            <li class="tab" onclick="openTab(event,'Details')"><a>Music</a></li>
-                            <li class="tab" onclick="openTab(event,'Specification')"><a>Videos</a></li>
-                            <li class="tab" onclick="openTab(event,'Reviews')"><a>Documents</a></li>
-                          </ul>
+                    <div className="column is-4">
+                        <div class="card">
+                          <div class="card-image">
+                            <figure class="image is-4by3">
+                              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                            </figure>
+                          </div>
+                          <div class="card-content">
+                            <div class="media">
+                              <div class="media-left">
+                                <figure class="image is-48x48">
+                                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
+                                </figure>
+                              </div>
+                              <div class="media-content">
+                                <p class="title is-4">John Smith</p>
+                                <p class="subtitle is-6">@johnsmith</p>
+                              </div>
+                            </div>
+
+                            <div class="content">
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                              <a href="#">#css</a> <a href="#">#responsive</a>
+                              <br/>
+                              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                    <div className="column is-4">
+                        <div class="card">
+                          <div class="card-image">
+                            <figure class="image is-4by3">
+                              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                            </figure>
+                          </div>
+                          <div class="card-content">
+                            <div class="media">
+                              <div class="media-left">
+                                <figure class="image is-48x48">
+                                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
+                                </figure>
+                              </div>
+                              <div class="media-content">
+                                <p class="title is-4">John Smith</p>
+                                <p class="subtitle is-6">@johnsmith</p>
+                              </div>
+                            </div>
+
+                            <div class="content">
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                              <a href="#">#css</a> <a href="#">#responsive</a>
+                              <br/>
+                              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                    <div className="column is-4">
+                        <div class="card">
+                          <div class="card-image">
+                            <figure class="image is-4by3">
+                              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                            </figure>
+                          </div>
+                          <div class="card-content">
+                            <div class="media">
+                              <div class="media-left">
+                                <figure class="image is-48x48">
+                                  <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image"/>
+                                </figure>
+                              </div>
+                              <div class="media-content">
+                                <p class="title is-4">John Smith</p>
+                                <p class="subtitle is-6">@johnsmith</p>
+                              </div>
+                            </div>
+
+                            <div class="content">
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                              <a href="#">#css</a> <a href="#">#responsive</a>
+                              <br/>
+                              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                            </div>
+                          </div>
                         </div>
                     </div>
                 </section>
-                <div className="container section">
-                    <div id="Overview" className="tabcontent" >
-                      <p>
-                      Overview Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    </div>
-                    <div id="Details" class="tabcontent" style={{display: 'none'}}>
-                        <p>
-                          test2<br />
-                        Details Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    </div>
-                    <div id="Specification" className="tabcontent" style={{display: 'none'}}>
-                       <p>  
-                       Specification Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    </div>
-                    <div id="Reviews" className="tabcontent" style={{display: 'none'}}>
-                       <p> 
-                       Specification Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrs standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    </div>
-                </div>
             </Layout>
         )
     }
