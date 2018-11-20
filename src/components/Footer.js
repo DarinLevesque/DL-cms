35998d385a2a964937f65bc9dab9dbd952d8423f
Darin Levesque <darin@darinlevesque.com>
2018-11-20 14:49:52 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,12 +43,6 @@ const Footer = () => {
             <div className="column is-one-third">
               <p>Contact Me...</p>
               <br/>
-              <OutboundLink href='https://ko-fi.com/E1E4LU0A' target='_blank'>
-                <img height='36'
-                  style='border:0px;height:36px;' 
-                  src='https://az743702.vo.msecnd.net/cdn/kofi4.png?v=0'
-                  border='0'
-                  alt='Buy Me a Coffee' />
               </OutboundLink>
               <br/>          
               <OutboundLink href="https://bulma.io">
