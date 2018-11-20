c928ce5b1d93ac107e492ea63f890e1ccbd445c6
Darin Levesque <darin@darinlevesque.com>
2018-11-20 15:30:02 -0500

    Update experience.js

--- src/pages/resume/experience.js
+++ src/pages/resume/experience.js
@@ -18,8 +18,8 @@ const Experience = () => {
         <div className="timeline-item is-primary">
           <div className="timeline-marker is-primary"></div>
           <div className="timeline-content">
-            <p className="heading">January 2016</p>
-            <p>Timeline content - Can include any HTML element</p>
+            <p className="heading">September 2015</p>
+            <p>Hired into Government service as a Test Resource Manager <TimeAgo date="Sep 30, 2015"/>.</p>
           </div>
         </div>
         <div className="timeline-item is-warning">
@@ -43,19 +43,7 @@ const Experience = () => {
             <p>Timeline content - Can include any HTML element</p>
           </div>
         </div>
-    <header className="timeline-header">
-          <span className="tag is-primary">2017</span>
-        </header>
-        <div className="timeline-item is-danger">
-          <div className="timeline-marker is-danger is-icon">
-            <i className="fa fa-flag"></i>
-          </div>
-          <div className="timeline-content">
-            <p className="heading">January 2004</p>
-            <p>Timeline content - Can include any HTML element</p>
-          </div>
-        </div>
-    <header className="timeline-header">
+        <header className="timeline-header">
           <span className="tag is-primary">2017</span>
         </header>
         <div className="timeline-item">
@@ -65,6 +53,27 @@ const Experience = () => {
           <div className="timeline-content">
             <p className="heading">March 2006</p>
             <p>Transitioned to V-22 Osprey</p>
+          </div>
+        </div>
+        <div className="timeline-item">
+          <div className="timeline-marker is-danger is-icon">
+            <i className="fa fa-flag"></i>
+          </div>
+          <div className="timeline-content">
+            <p className="heading">Iraq Deployment</p>
+            <p>HMM-266 CH-46E deployment to Al Asad</p>
+          </div>
+        </div>
+        <header className="timeline-header">
+          <span className="tag is-primary">2005</span>
+        </header>
+        <div className="timeline-item">
+          <div className="timeline-marker is-danger is-icon">
+            <i className="fa fa-flag"></i>
+          </div>
+          <div className="timeline-content">
+            <p className="heading">January 2004</p>
+            <p>Joined Marine Corps as a CH-46E Crew Chief</p>
           </div>
         </div>
         <header className="timeline-header">
